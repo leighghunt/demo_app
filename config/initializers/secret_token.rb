1b0423e4dd91c9b043a514c07e98a76060b9b1e4
Leigh Hunt <leigh.hunt@splicegroup.co.nz>
2012-07-14 12:00:11 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c8c08ded02917a0db3fc3585f3dce228c306a240b17952e646c802288ee29358fec2982f12178ae3fdd3540639a0639f83ab253e85ed6acc515b06eff588777e'
